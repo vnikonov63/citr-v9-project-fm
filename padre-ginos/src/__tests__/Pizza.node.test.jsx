79f598a516d5c6f33caeeef1f1e8cc054e2896b6
Vasilii Nikonov <nikonov.va@yandex.ru>
2025-02-05 16:39:16 -0800

    Practiced creating tests with the use if vitest and other tools

---
+++ padre-ginos/src/__tests__/Pizza.node.test.jsx
@@ -0,0 +1,25 @@
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
+import Pizza from "../Pizza";
+
+afterEach(cleanup);
+
+test("alt text renders on image", async () => {
+  const name = "My Favorite Pizza";
+  const src = "https://picsum.photos/200";
+  const screen = render(
+    <Pizza name={name} description="super cool pizza" image={src} />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+});
+
+test("to have a default image if none is provided", async () => {
+  const screen = render(
+    <Pizza name={"something else"} description="super cool pizza" />,
+  );
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
+});
